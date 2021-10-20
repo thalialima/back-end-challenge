71a7a3b4a1007088d009739eba534526fe4e14e4
thalialima <thaliairene20@gmail.com>
2021-10-20 14:21:51 -0300

    Requisição DELETE que apaga um vídeo peo ID

--- src/main/java/br/com/alura/aluraflix/controller/VideosController.java
+++ src/main/java/br/com/alura/aluraflix/controller/VideosController.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,61 +29,64 @@ import br.com.alura.aluraflix.repository.VideoRepository;
 @RestController
 @RequestMapping("/videos")
 public class VideosController {
-	
+
 	@Autowired
 	private VideoRepository videoRepository;
 
-	
 	@GetMapping
 	public List<VideoDTO> showVideos() {
 		List<Video> videos = videoRepository.findAll();
 		return VideoDTO.toVideosDTO(videos);
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<VideoDTO> details(@PathVariable Long id){
+	public ResponseEntity<VideoDTO> details(@PathVariable Long id) {
 		Optional<Video> video = videoRepository.findById(id);
-		
-		//fail fast
-		if(!video.isPresent())
+
+		// fail fast
+		if (!video.isPresent())
 			return ResponseEntity.notFound().build();
-		
+
 		return ResponseEntity.ok(new VideoDTO(video.get()));
 	}
-	
+
 	@PostMapping
 	@Transactional
-	public ResponseEntity<VideoDTO> 
-	register(@RequestBody @Valid VideoForm form,
-			UriComponentsBuilder urBuilder) {
-		
+	public ResponseEntity<VideoDTO> register(@RequestBody @Valid VideoForm form, UriComponentsBuilder urBuilder) {
+
 		Video video = form.toVideo();
 		videoRepository.save(video);
-		
+
 		URI uri = urBuilder.path("/videos/{id}").buildAndExpand(video.getId()).toUri();
 		return ResponseEntity.created(uri).body(new VideoDTO(video));
-		
+
 	}
 
-	
 	@PutMapping("/{id}")
 	@Transactional
-	public ResponseEntity<VideoDTO> update(@PathVariable Long id, 
-			@RequestBody @Valid VideoForm videoForm){
-		
+	public ResponseEntity<VideoDTO> update(@PathVariable Long id, @RequestBody @Valid VideoForm videoForm) {
+
 		Optional<Video> optional = videoRepository.findById(id);
-		
-		if(optional.isPresent()) {
+
+		if (optional.isPresent()) {
 			Video video = videoRepository.save(videoForm.update(id, videoRepository));
 			return ResponseEntity.ok(new VideoDTO(video));
 		}
 
 		return ResponseEntity.notFound().build();
-		
 	}
-	
-	
-	
-	
-	
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+
+		Optional<Video> optional = videoRepository.findById(id);
+		if (optional.isPresent()) {
+			videoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
 }
